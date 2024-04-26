b4aad643653d01495cadf2616259042437cc9eea
ErikaCibellySx24 <105865954+ErikaCibellySx24@users.noreply.github.com>
2024-04-26 17:49:15 -0300

    Templates (up)

--- djangoPy/settings.py
+++ djangoPy/settings.py
@@ -13,8 +13,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,8 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'ENFORCE_SCHEMA': False,
-        'NAME': '<database_name>',
-        'ENFORCE_SCHEMA': False,
+        'NAME': 'cluster0',  
         'CLIENT': {
             'host': 'cluster0.mongodb.net',
             'username': 'erikacibelly034',
@@ -90,6 +94,7 @@ DATABASES = {
         }
     }
 }
+
 
 
 
